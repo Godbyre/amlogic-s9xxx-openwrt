ed915f1df7596bbe9a80b2df7f46c5c5ca5dfce3
Godbyre <116703988+Godbyre@users.noreply.github.com>
2025-05-17 15:22:21 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -17,8 +17,8 @@ echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.defa
 echo >> feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
-echo >> feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small;master' >> feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages;master' >> feeds.conf.default
+# echo >> feeds.conf.default
+# echo 'src-git small https://github.com/kenzok8/small;master' >> feeds.conf.default
+# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages;master' >> feeds.conf.default
 
 
